3152fe81c5ba3559fe357782ad1b2f65791d5e73
Mike Innes <mike.j.innes@gmail.com>
2013-07-11 06:36:48 +0100

    set version 0.1.0, project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
-(defproject clarity "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clarity "0.1.0"
+  :description "Better S-Expressions"
+  :url "https://github.com/one-more-minute/clarity/"
   :dependencies [[org.clojure/clojure "1.5.1"]])
