f5945dbda9652b98a51b141f935caa7d4eef3e46
Mike Innes <mike.j.innes@gmail.com>
2013-07-11 09:47:35 +0100

    inc version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clarity "0.1.0"
+(defproject clarity "0.2.0-SNAPSHOT"
   :description "Better S-Expressions"
   :url "https://github.com/one-more-minute/clarity/"
   :dependencies [[org.clojure/clojure "1.5.1"]])
