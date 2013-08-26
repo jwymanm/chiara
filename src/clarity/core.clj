0559dc27ad9e0687564580ec1e21db049c192022
Mike Innes <mike.j.innes@gmail.com>
2013-08-26 16:35:40 +0100

    add line/column metadata

--- src/clarity/core.clj
+++ src/clarity/core.clj
@@ -4,13 +4,16 @@
         clarity.syntax
         clarity.utils))
 
-(defn seq' [form]
-  (if (= (count form) 1)
-    (first form)
-    (apply list form)))
-
-(defn discard-line [reader]
-  (while (not= (read-1 reader) \newline)))
+(defn seq'
+  ([form]
+    (if (= (count form) 1)
+      (first form)
+      (apply list form)))
+  ([form meta]
+    (let [form (seq' form)]
+      (if (instance? clojure.lang.IMeta form)
+        (with-meta form meta)
+        form))))
 
 (defn read-whitespace [reader]
   (loop [count 0
@@ -27,26 +30,28 @@
 (defn read-clarity
   ([reader] (first (read-clarity reader (first (read-whitespace reader)))))
   ([reader indent]
-    (loop [form [(read-next reader)]
-           [next-i newline] (read-whitespace reader)]
-      (if (peek reader)
-        (if newline
-          (if (> next-i indent)
-            (let [[next-form next-i] (read-clarity reader next-i)]
-              (recur (conj form next-form) [next-i true]))
-            [(seq' form) next-i])
-          (recur (conj form (read-next reader)) (read-whitespace reader)))
-        [(seq' form) 0]))))
+    (let [[l c] (reader-position reader)]
+      (loop [form [(read-next reader)]
+             [next-i newline] (read-whitespace reader)]
+        (if (peek reader)
+          (if newline
+            (if (> next-i indent)
+              (let [[next-form next-i] (read-clarity reader next-i)]
+                (recur (conj form next-form) [next-i true]))
+              [(seq' form {:line l :column c}) next-i])
+            (recur (conj form (read-next reader)) (read-whitespace reader)))
+          [(seq' form {:line l :column c}) 0])))))
 
+;; Catch EOF here
 (defn read-to-bracket [reader]
   (read-whitespace reader)
   (if (= (str (peek reader)) ")")
     (do (read-1 reader) ())
     (let [next (read-clarity reader)]
-      (conj (read-all reader) next))))
+      (conj (read-to-bracket reader) next))))
 
 (defsyntax ^:stream clarity [reader]
-  (-> reader read-all (conj 'do)))
+  (-> reader read-to-bracket (conj 'do)))
 
 (defn use-clarity
   ([] (use-clarity true))
